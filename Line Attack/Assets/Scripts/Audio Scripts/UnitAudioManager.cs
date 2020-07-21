12ca0e951ac2f3b15fcd87ea651ac25ecabc77ac
Hauntlud <hauntled@gmail.com>
2020-07-21 11:10:01 +0100

    Audio/UIScreens

---
+++ Line Attack/Assets/Scripts/Audio Scripts/UnitAudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class UnitAudioManager : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+	public AudioClip drawSwordClip;
+	public AudioClip swordCollideClip;
+	public AudioClip drawFireClip;
+	public AudioClip casteSpellClip;
+
+	AudioSource audioSource;
+
+	void Start()
+    {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void DrawSwordSound()
+	{
+
+	}
+
+	public void SwordCollideSound()
+	{
+
+	}
+
+	public void DrawFireSound()
+	{
+
+	}
+
+	public void CasteSpell()
+	{
+
+	}
+}
---
+++ Line Attack/Assets/Scripts/UI Scripts/MainMenuUIScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuUIScript : MonoBehaviour
+{
+	public void LoadLevel()
+	{
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
