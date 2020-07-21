91a260fc1ad02a8a997c52d0c9304cf83d11ea18
VikingJesus <Gobber121@googlemail.com>
2020-07-21 09:18:16 +0100

    Loaded from backuup

---
+++ Line Attack/Assets/Scripts/UI Scripts/WinLooseUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinLooseUI : MonoBehaviour
+{
+	public void RestartLevel()
+	{
+		Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
